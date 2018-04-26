c62f45fa411b0b20a14064253442d86b108b8c91
dx100cam <dx100@cam.ac.uk>
2018-04-26 17:38:57 +0100

    exercise4.py changed for lesson 5 -- with data augment.

--- DanB/dx_code/exercise4.py
+++ DanB/dx_code/exercise4.py
@@ -1,4 +1,4 @@
-""" Python kernel for exercise in Lesson 4, transfer learning."""
+""" Python kernel for exercise in Lesson 4 and 5, transfer learning."""
 from tensorflow.python.keras.applications import ResNet50
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
@@ -23,15 +23,20 @@ image_size = 224
 # The ImageDataGenerator was previously generated with
 # data_generator = ImageDataGenerator(preprocessing_function=preprocess_input)
 # recent changes in keras require that we use the following instead:
-data_generator = ImageDataGenerator()
+data_generator_with_aug = ImageDataGenerator(
+                            horizontal_flip= True,
+                            width_shift_range=0.2,
+                            height_shift_range=0.2)
 
-train_generator = data_generator.flow_from_directory(
+data_generator_no_aug = ImageDataGenerator()
+
+train_generator = data_generator_with_aug.flow_from_directory(
     '../input/dogs-gone-sideways/images/train',
     target_size=(image_size, image_size),
-    batch_size=24,
+    batch_size=12,
     class_mode='categorical')
 
-validation_generator = data_generator.flow_from_directory(
+validation_generator = data_generator_no_aug.flow_from_directory(
     '../input/dogs-gone-sideways/images/val',
     target_size=(image_size, image_size),
     class_mode='categorical')
@@ -39,6 +44,6 @@ validation_generator = data_generator.flow_from_directory(
 
 my_new_model.fit_generator(
     train_generator,
-    steps_per_epoch=3,
-    validation_data=validation_generator,
-    validation_steps=1)
+    epochs=3,
+    steps_per_epoch=19,
+    validation_data=validation_generator)
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# MedImageProcess
-DenB: 		Online tutorial for tensorflow + keras (https://www.kaggle.com/learn/deep-learning)
+# Repo is MedImageProcess
+DSB2017:    Data Science Bowl 2017
+DanB: 		Online tutorial for tensorflow + keras (https://www.kaggle.com/learn/deep-learning)
